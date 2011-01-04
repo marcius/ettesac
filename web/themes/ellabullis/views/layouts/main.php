6e401a56ca258a5b982d745618b80dea446142f7
marcius <marcius@831bacd9-606d-4f6a-aa67-a4de6323e4d2>
2011-01-04 11:24:06 +0000

    git-svn-id: https://ettesac.svn.sourceforge.net/svnroot/ettesac/trunk@12 831bacd9-606d-4f6a-aa67-a4de6323e4d2

--- web/themes/ellabullis/views/layouts/main.php
+++ web/themes/ellabullis/views/layouts/main.php
@@ -27,7 +27,7 @@
                     </div>
                     <div id="navbar">
                         <div><a href="/">Home</a><span></span></div>
-                        <div><a href="#">Tariffe</a><span></span></div>
+                        <div><a href="/site/rates">Tariffe</a><span></span></div>
                         <div><a href="#">Contatti</a><span></span></div>
                         <div><a href="#">Prenota</a><span></span></div>
                         <div class="clear"></div>
