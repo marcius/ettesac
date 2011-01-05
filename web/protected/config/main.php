1fbb0a3373b94d832bd5e59a639361e84d6599d1
marcius <marcius@831bacd9-606d-4f6a-aa67-a4de6323e4d2>
2011-01-05 00:40:25 +0000

    git-svn-id: https://ettesac.svn.sourceforge.net/svnroot/ettesac/trunk@16 831bacd9-606d-4f6a-aa67-a4de6323e4d2

--- web/protected/config/main.php
+++ web/protected/config/main.php
@@ -85,6 +85,6 @@ return array(
     // using Yii::app()->params['paramName']
     'params' => array(
         // this is used in contact page
-        'adminEmail' => 'webmaster@example.com',
+        'adminEmail' => 'info@lecasettedilulu.it',
     ),
 );
